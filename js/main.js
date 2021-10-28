43251dd1f68775ca416f2b321efb9f933ab507af
Chad Kluck <clkluck@stthomas.edu>
2021-10-27 21:08:43 -0500

    cleaned up code, made standard code more compact

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@
 	even if it removes attribution comments.
 
 	Created from template:
-		Chad Leigh Kluck - 10/15/2018
-		Version: 0.0.1-20181015-01
+		Chad Leigh Kluck - 10/27/2021
+		Version: 0.0.2-20211027-01
 		chadkluck.net
 		github.com/chadkluck/js-template
 
@@ -33,7 +33,7 @@
 
 // thistemplate - change to a name for your script, also change at very end inside the ()
 if (typeof thistemplate === 'undefined') { thistemplate = false; } // let init take care of setting true
-if (typeof thistemplate_config === 'undefined') { thistemplate_config = null; } // let init take care of setting true
+if (typeof thistemplate_config === 'undefined') { thistemplate_config = {}; } // let init take care of setting true
 
 
 /*  ============================================================================================
@@ -42,15 +42,15 @@ if (typeof thistemplate_config === 'undefined') { thistemplate_config = null; } 
 	********************************************************************************************
 */
 
-(function( myInit , configParam ) {
+(function( myInit , pConfig ) {
 
 	"use strict";
 
 /* *** Local variables *** */
 
 	/* Just version and credits that will show in console log */
-	var info = {
-		version: "0.0.1-20181015-01", // just a manual version number for debugging: "Is it loading the code I *thought* I uploaded?" Recommend 0.0.0-YYYYMMDD-00 format
+	const info = {
+		version: "0.0.1-20211027-01", // just a manual version number for debugging: "Is it loading the code I *thought* I uploaded?" Recommend 0.0.0-YYYYMMDD-00 format
 		handle:  "[YOURSCRIPT]", // the uppercase short handle that shows in console log
 		name: 	 "[Your Script Name]", // the name of the script
 		author:  "[Your Name]", // author or organization credited with writing it
@@ -58,13 +58,14 @@ if (typeof thistemplate_config === 'undefined') { thistemplate_config = null; } 
 	};
 
 	// this can eithr be passed in as the second param (very bottom) or set here
-	var configDefault = {
+	const configDefault = {
 		silence: { allowToggle: true, default: false },
 		allowMultipleExecutions: false, // no reason to ever set this as true
 		apiURL: "https://api.chadkluck.net/games/", // set this to the location of the api
 	};
 
-	const CONFIG = configParam !== null ? configParam : configDefault;
+	/** Script Config - contains values from configDefault, with any overrides passed in from pConfig */
+	const CONFIG = Object.assign(configDefault, pConfig);
 
 	/* Runtime Settings (Read/Write) */
 	var settings = {
@@ -75,119 +76,77 @@ if (typeof thistemplate_config === 'undefined') { thistemplate_config = null; } 
 /* *** Local Functions *** */
 
 
-	/* =====================================================================
-	 *  init()
-	 *
-	 *  Initial function called at runtime
-	*/
-	var init = function(){
+	/* ===================================================================== */
+
+	/**
+	 * Inital function called at runtime which initializes the function stating
+	 * that the function has loaded in the console (provided not silenced) and
+	 * then calls the execute function
+	 * 
+	 * No need to modify
+	 */
+	const init = function(){
+		const getGlobalInit = function() { return myInit; };
+		const setGlobalInit = function(b) { myInit = b === true ? true : false; /* don't blindly accept what is passed */ return getGlobalInit(); };
+		const setSilence = function(silence){
+			if ( silence !== settings.silent ) {
+				if (CONFIG.silence.allowToggle ) {
+					if( silence ) { debug("Silenced"); settings.silent = true; /* we do it last so that there was one final peep */ } else { settings.silent = false; debug("Unsilenced"); }
+				} else {
+					settings.silent = CONFIG.silence.default;
+				}
+			}
+		};
+		const attribution = function(){ debug("Loading " + info.name + " by " + info.author); debug("Version " + info.version); if(info.code !== "") { debug("Get Code: " + info.code); } };
 		// check to see if it has already initialized or if another copy of the code has already ran
 		if( !getGlobalInit() || (getGlobalInit() && CONFIG.allowMultipleExecutions ) ) {
-			setGlobalInit(true);
-			attribution();
-			setSilence(CONFIG.silence.default);
-			execute();
+			setGlobalInit(true); attribution(); setSilence(CONFIG.silence.default); execute();
 		}
-
 	};
 
-	/* =====================================================================
-	 *	debug()
-	 *
-	 *	If not silenced, outputs text passed to it to console.log
-	 *
-	 *	Need a line number? In your code use debug(yourmessage + " - Line:"+ (new Error()).lineNumber );
-	 *
-	 *	This function has a companion variable: silent
-	 */
-	var debug = function( text ) {
+	/* ===================================================================== */
 
+	/**
+	 * If not silenced, outputs text passed to it to console.log
+	 * Need a line number? In your code use debug(yourmessage + " - Line:"+ (new Error()).lineNumber );
+	 * This function has a companion variable: silent
+	 * 
+	 * No need to modify 
+	 * 
+	 * @param {string} text 
+	 */
+	const debug = function( text ) {
+		const pad = function (num, size = 2) { num = num.toString(); while (num.length < size) num = "0" + num; return num; }
 		// as long as we aren't silenced (silent === false)...
-		if( !settings.silent ) {
-			var d = new Date();
-			var ts = d.getHours() +":"+ d.getMinutes() +":"+ d.getSeconds() +"."+ d.getMilliseconds();
-			console.log(info.handle+" ["+ts+"] : " + text);
-		}
-	};
-
-
-	/* =====================================================================
-	 *  setSilence()
-	 *
-	 *  If silenced, debug() won't send messages to console.log
-	*/
-	var setSilence = function(silence){
-		if ( silence !== settings.silent ) {
-			if (CONFIG.silence.allowToggle ) {
-				if( silence ) {
-					debug("Silenced");
-					settings.silent = true; // we do it last so that there was one final peep
-				} else {
-					settings.silent = false;
-					debug("Unsilenced");
-				}
-			} else {
-				settings.silent = CONFIG.silence.default;
-			}
-		}
-
-	};
-
-	/* =====================================================================
-	 *  attribution()
-	 *
-	 *  Display info about the script in the command line
-	*/
-	var attribution = function(){
-		debug("Loading " + info.name + " by " + info.author);
-		debug("Version " + info.version);
-		if(info.code !== "") { debug("Get Code: " + info.code); }
-	};
-
-	/* =====================================================================
-	 *  getGlobalInit() / setGlobalInit()
-	 *
-	 */
-	var getGlobalInit = function() {
-		return myInit;
-	};
-
-	var setGlobalInit = function(b) {
-		myInit = b === true ? true : false; // don't blindly accept what is passed
-		return getGlobalInit();
-	};
-
-	/* =====================================================================
-	 *  API functions to get remote JSON data
-	 */
-	var xhrSuccess = function() { this.callback.apply(this, this.arguments); };
-
-	var xhrError = function() { console.error(this.statusText); };
-
-	var loadFile = function(sURL, fCallback) {
-		var oReq = new XMLHttpRequest();
-		oReq.callback = fCallback;
-		oReq.arguments = Array.prototype.slice.call(arguments, 2);
-		oReq.onload = xhrSuccess;
-		oReq.onerror = xhrError;
-		oReq.open("get", sURL, true);
-		oReq.send(null);
-	};
-
-	var getAPI = function(url, display) {
-
-		// Define the function we want to use to process the data, accepting a callback function as a parameter (which will be pased to it later)
-		var process = function(callback) {
-			var data = JSON.parse(this.responseText);
-			callback(data);
-		}; // end callback processing function
-
-		// The actual call to the loadFile, passing the two functions we wish to execute
-		loadFile (url, process, display);
+		if( !settings.silent ) { let d = new Date(); let ts = pad(d.getHours()) +":"+ pad(d.getMinutes()) +":"+ pad(d.getSeconds()) +"."+ pad(d.getMilliseconds(),3); console.log(info.handle+" ["+ts+"] : " + text); }
 	};
 
 	/* ****************************************************************************
-	 * EXECUTION
+	 * API FUNCTIONS
+	 * ****************************************************************************
+
+		Can be removed if no API is called
+		No need to modify the functions
+
+		Usage:
+
+		getAPI(apiEndpointURL, callbackFunction);
+
+	 * ************************************************************************** */
+
+	const xhrSuccess = function() { this.callback.apply(this, this.arguments); };
+	const xhrError = function() { console.error(this.statusText); };
+	const loadFile = function(sURL, fCallback) { let  oReq = new XMLHttpRequest(); oReq.callback = fCallback; oReq.arguments = Array.prototype.slice.call(arguments, 2); oReq.onload = xhrSuccess; oReq.onerror = xhrError; oReq.open("get", sURL, true); oReq.send(null); };
+
+	/**
+	 * Connect to an api and then have a callback function do something with it
+	 * @param {string} url The endpoint to contact
+	 * @param {*} callBackFunction The function to pass API data to and execute after API data is recieved.
+	 */
+	const getAPI = function(url, callBackFunction) { const process = function(callback) { let data = JSON.parse(this.responseText); callback(data); }; loadFile (url, process, callBackFunction); };
+
+	/* ****************************************************************************
+	 * EXECUTION FUNCTIONS
 	 * ****************************************************************************
 
 		Function that runs at execution time, invoked by init()
@@ -195,7 +154,7 @@ if (typeof thistemplate_config === 'undefined') { thistemplate_config = null; } 
 
 	 * ************************************************************************** */
 
-	var execute = function() {
+	const execute = function() {
 
 		// This is where you add all your functions. If using APIs don't forget to declare a function that will be excuted after the api data is returned
 		// showData() is provided as an example
@@ -206,20 +165,25 @@ if (typeof thistemplate_config === 'undefined') { thistemplate_config = null; } 
 			bar: 2
 		};
 
-		var showData = function(data) {
+		const showData = function(data) {
 			// display any data returned from an API
 
 			// Code example to get you started
 
 			// ----- BEGIN API CODE EXAMPLE USING DATA RETURNED FROM api.chadkluck.net/games
+			// ----- This example code will create an unordered list element and display a list of games retreived from an api
 
-			// create a div to put it in
-			var ul = document.createElement("ul");
+			// create an unordered list to put the list in
+			let ul = document.createElement("ul");
 
-			data.gamechoices.forEach(function(element) {
-  				var li = document.createElement("li");
-  				li.innerHTML = element;
-  				ul.appendChild(li);
+			/* In this example we are expecting data = { gamechoices: [] }
+			so we will treat data.gamechoices[] as an array and do a foreach
+			*/
+
+			data.gamechoices.forEach(function(game) {
+  				let li = document.createElement("li");
+  				li.innerHTML = game;
+  				ul.appendChild(li); // add the list item to the list
 			});
 
 			// append to end of body
@@ -233,7 +197,6 @@ if (typeof thistemplate_config === 'undefined') { thistemplate_config = null; } 
 		// call API and after data is returned, show it
 		getAPI(CONFIG.apiURL, showData);
 
-
 	};
 
 
@@ -246,6 +209,7 @@ if (typeof thistemplate_config === 'undefined') { thistemplate_config = null; } 
 
 	 * ************************************************************************** */
 
+	/* Initializes then executes */
 	init();
 
 })(thistemplate, thistemplate_config);
